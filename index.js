870e5cff67991747502d6d1d679e8fda0da96dae
Nick Weiland <njweiland23@gmail.com>
2023-09-19 16:26:45 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(){
+    console.log("HELLO")
+}
+function logWhisper() {
+    console.log("hello")
+}
+function sayHiToHeadphonedRoommate(string) {   
+   {const string = "hello"
+    console.log(string.toLowerCase())
+    console.log(string.toUpperCase())}
+   if (string == string.toLowerCase()) {
+    return `I can't hear you!`
+   } else if (string == string.toUpperCase()) {
+    return `YES INDEED!`
+   } else if (string == `Let's have dinner together!`) {
+    return `I would love to!`
+   }
+}
